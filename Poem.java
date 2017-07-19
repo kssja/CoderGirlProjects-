e39469180c406d2381e5f80277fc652f7bb461e1
kssja <30272323+kssja@users.noreply.github.com>
2017-07-18 19:00:13 -0500

    A Poem: Practice with printing to the screen

---
+++ Poem.java
@@ -0,0 +1,47 @@
+import static java.lang.System.out;
+
+public class Poem 
+{
+public static void main (String[] args)
+{
+System.out.println("JABBERWOCKY"
+ + "\n'Twas brillig, and the slithy toves"
+ + "\n   Did gyre and gimble in the wabe;"
+ + "\n All mimsy were the borogoves,"
+ + "\n    And the mome raths outgrabe."
+ + "\n\n \"Beware the Jabberwock, my son!"
+ + "\n    The jaws that bite, the claws that catch!"
+ + "\n Beware the Jubjub bird, and the shun"
+ + "\n    The frumoius Bandersnatch!"
+ + "\n\n He took his vorpal sword in hand:"
+ + "\n    Long time the manxome foe he sought - "
+ + "\n So rested he by the Tumtum tree,"
+ + "\n    And stood awhile in thought."
+ + "\n\n And as in uffish thought he stood,"
+ + "\n    The Jabberwock, with eyes of flame,"
+ + "\n Came whiffling through the tulgey wood,"
+ + "\n    And burbled as it came!"
+ + "\n\n One, two! One, two! And through and through"
+ + "\n    The vorpal blade went snicker-snack!"
+ + "\n He left it dead, and with its head"
+ + "\n    He went galumphing back." 
+ + "\n\n \"And hast thou slain the Jabberwock?"
+ + "\n     Come to my arms, my beamish boy!"
+ + "\n O frabjous day! Callooh! Callay!\" "
+ + "\n      He chortled in his joy."
+ + "\n\n 'Twas brillig, and the slithy toves"
+ + "\n      Did gyre and gimble in the wabe;"
+ + "\n All mimsy were the borogoves,"
+ + "\n      And the mome raths outgrabe."
+ +"\n\n \"It seems very pretty,\" she said when she had finished"
+ + "\n it, \"but it's RATHER hard to understand!\" (You see"
+ + "\n she didn't like to confess, ever to herself, that "
+ + "\n she couldn't make it out at all.) \"Somehow it seems"
+ + "\n to fill my head with ideas - only I don't exactly "
+ + "\n know what they are! However, SOMEBODY killed"
+ + "\n SOMETHING: that's clear, at any rate.\" ");
+ 
+  
+ 
+}    
+}
