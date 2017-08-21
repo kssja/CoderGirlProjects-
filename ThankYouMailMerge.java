67ec9dff168a581d54762c938acab82df05f6455
kssja <30272323+kssja@users.noreply.github.com>
2017-08-21 14:27:15 -0500

    Add files via upload
    Monday upload 2:27 pm

---
+++ ThankYouMailMerge.java
@@ -0,0 +1,57 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+import java.text.NumberFormat;
+
+public class ThankYouMailMerge{
+
+
+public static void main(String[] args){
+    double amt= 0;
+    String name="";
+    Scanner keyboard = new Scanner(System.in);
+    name="";
+    out.println("Thank You Letter");
+    NumberFormat fmt = NumberFormat.getCurrencyInstance();
+    boolean keepGoing = true;
+    
+    
+    while(keepGoing){
+    out.println("\nPlease enter your first and last name  \n ");
+    
+    name = keyboard.nextLine();
+    
+    out.println("How much would you like to contribute?");
+    amt = keyboard.nextDouble();
+ 
+    
+    out.println(
+    
+    "\n\nDear " + name + "," +
+    
+    "\nThank you for your donation!  We rely on donors like you to keep our" +
+    "\norganization effective, and you came through for us.  Your donation of "
+    +  fmt.format(amt) + 
+    "\nwill help our efforts to make a difference in the world."+
+    
+    "\n\nAs you may know, we are a registered non-profit organization,"+ 
+    "\nso your dontation is tax deductible.  You may use this letter" +
+    "\nas a receipt for tax purposes. " +
+    
+    "\n\nThank you again for your support!"+
+    
+    "\n\nSincerely," +
+    "\nPaula Jones" +
+    "\nYour Charity.org");
+    
+    out.print("\n\nDo you have more letters to send?"+
+    "\nEnter Yes for more letters or quit to quit  ");
+       
+    name = keyboard.nextLine();
+    name = keyboard.nextLine();
+    if(name.equalsIgnoreCase("quit")){
+        keepGoing = false;
+    }else
+    keepGoing = true;
+}
+}
+}
