7e6969ad0e2cb289a900f263a879db187a19b183
kssja <30272323+kssja@users.noreply.github.com>
2017-09-11 17:58:49 -0500

    Update GuessMyNumber.java
    updated to count data entry errors as guesses

--- GuessMyNumber.java
+++ GuessMyNumber.java
@@ -29,23 +29,30 @@ public class GuessMyNumber{
             int guess = 0;
 
             while(entry.equalsIgnoreCase("yes")&& (count!= 6 && count1!=6)){
-                
 
                 if(pn == true){
                     nm = name;
 
                 }else nm = name2;
-
                 out.println("\n"+ nm + " ,what is your guess?");
                 guess = keyboard.nextInt();
 
-                while( (guess <1 || guess>100)){
+                if( (guess <1 || guess>100)){
                     System.out.println("\n" + nm +"  ,your number is not between 1 and 100."  + 
-                        "please try another number.");
-                    guess=keyboard.nextInt();
+                        "Your turn is over.");
 
+                    if (pn == true){
+                        count = count +1;
+                        pn = false;
+                    }else{
+
+                        count1 = count1 +1; 
+
+                        pn=true;
+                    } 
                 }
-                if(guess < number){
+
+                if((guess>1 && guess<100) && guess < number){
                     System.out.println("\n" + nm + "  ,sorry your number is tooooo low!!  Your turn has ended ....better luck next time");
                     if (pn == true){
                         count = count +1;
@@ -56,7 +63,7 @@ public class GuessMyNumber{
                     }    
                 } 
 
-                if( guess > number){
+                if( (guess >1 && guess <100) && guess > number){
                     System.out.println("\n" + nm +" ,your number is too high!  Your turn is over....sorry");
                     if (pn == true){
                         count = count +1;
@@ -66,8 +73,8 @@ public class GuessMyNumber{
                         pn = true;
                     }
                 }
-            
-                if( guess == number){
+
+                if( (guess>1 && guess < 100) && guess == number){
 
                     System.out.println("\nPerfect "+ nm + "!! You guessed correctly!! ");
 
@@ -80,18 +87,15 @@ public class GuessMyNumber{
 
                 }
                 if(count ==5 && count1 ==5){
-            
-            out.println("\n" + name +", "+ name2 + "  Game over for you both.....need lessons on guessing????"+
-                "\n\nWould you like to play again?  If so answer Yes and make a guess or No to end the game");
-            entry = keyboard.nextLine();
-            entry = keyboard.nextLine();
-            number = num.nextInt(max);
-            count =0;
-            count1=0;
-        }
-          
-        }}
-    }
+
+                    out.println("\n" + name +", "+ name2 + "  Game over for you both.....need lessons on guessing????"+
+                        "\n\nWould you like to play again?  If so answer Yes and make a guess or No to end the game");
+                    entry = keyboard.nextLine();
+                    entry = keyboard.nextLine();
+                    number = num.nextInt(max);
+                    count =0;
+                    count1=0;
+                }}}}
 
 }
 
