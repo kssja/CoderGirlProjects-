76f13b3c0996ada98046fd73e0e6ff7f9def74d4
kssja <30272323+kssja@users.noreply.github.com>
2017-10-17 22:00:25 -0500

    Add files via upload
    I hope I uploaded this correctly...made two classes User and Post and a main microblog

---
+++ User.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class User
+
+{
+    public String userName, url, firstName,lastName, emailAddr, post, a, b, c, d, e;
+
+    public User( String url, String userName, String firstName, String lastName, String emailAddr)
+    {
+        this.url = url;
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddr = emailAddr;
+    }
+
+    
+    
+    public String getuserName()
+    {
+        return this.userName;
+
+    }
+
+    public String getfirstName(){
+
+        return this.firstName;
+
+    }
+
+    public String getlastName()
+    {
+        return this.lastName;
+    }  
+
+    public String getUrl()
+    {
+        return this.url;
+    }
+
+    public String getemailAddr()
+    {
+        return this.emailAddr;
+    }
+
+}
